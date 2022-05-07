8b08eec6be212c1fb00eb88a1312a3ff4bb66016
Sambhav Sharma <devsambhav50@gmail.com>
2022-05-07 11:15:01 +0530

    add delete_record logic

--- SRM/main.c
+++ SRM/main.c
@@ -45,9 +45,9 @@ void menu()
         case 4:
             update_record();
             break;
-        // case 5:
-        //     delete_record();
-        //     break;
+        case 5:
+            delete_record();
+            break;
         case 6:
             exit(1);
         }
@@ -149,7 +149,7 @@ void search_record()
     {
         if (strcmp(sname, s.name) == 0)
         {
-            found += 1;
+            found = 1;
             printf("\n\t\t%-8d%-23s%-15s%-10d%-12s", i, s.name, s.mobile, s.roll, s.course);
             i++;
             break;
@@ -159,7 +159,7 @@ void search_record()
             found = 0;
         }
     }
-    if (found == 0)
+    if (!found)
     {
         printf("\n\t\tNo Record Found!!!");
     }
@@ -196,16 +196,16 @@ void update_record()
         {
             // getting data from user to update the record
             fflush(stdin);
-            printf("\n\t\tEnter Roll:- ");
+            printf("\n\t\tEnter New Roll:- ");
             scanf("%d", &s.roll);
             fflush(stdin);
-            printf("\t\tEnter Name:- ");
+            printf("\t\tEnter New Name:- ");
             scanf("%[^\n]", s.name);
             fflush(stdin);
-            printf("\t\tEnter Mobile:- ");
+            printf("\t\tEnter New Mobile:- ");
             scanf("%[^\n]", s.mobile);
             fflush(stdin);
-            printf("\t\tEnter Course:- ");
+            printf("\t\tEnter New Course:- ");
             scanf("%[^\n]", s.course);
             fseek(fp, -sizeof(s), SEEK_CUR);
             fwrite(&s, sizeof(s), 1, fp);
@@ -214,6 +214,62 @@ void update_record()
     }
     // close the file
     fclose(fp);
+    printf("\n\n\t\tPress any key to Continue...");
+    getch(); // hold the screen
+    menu();
+}
+
+// Delete record function
+void delete_record()
+{
+    FILE *fp, *temp;
+    student s;
+    char sname[20];
+    system("cls");
+    printf("\n\t---------------------------- UPDATE RECORDS -----------------------------\n");
+    printf("\n\t\tEnter name of Student:- ");
+    fflush(stdin);
+    scanf("%[^\n]", sname);
+    // open the file
+    fp = fopen("student_data.txt", "rb+");
+    // checking the file is exits or not
+    if (fp == NULL)
+    {
+        printf("\n\t\tCan't open this file!!!");
+        exit(1);
+    }
+    temp = fopen("temp_data.txt", "wb+");
+    if (temp == NULL)
+    {
+        printf("\n\t\tCan't open this file!!!");
+        exit(1);
+    }
+    int found = 0;
+    while (fread(&s, sizeof(s), 1, fp) == 1)
+    {
+        /// check given record is exists or not
+        if(strcmp(sname,s.name)==0){
+            found = 1;
+        }
+        // storing all the student which is not matched in temp file.
+        if (strcmp(sname, s.name) != 0)
+        {
+            fwrite(&s, sizeof(s), 1, temp);
+        }
+    }
+    if (found == 0)
+    {
+        printf("\n\t\tNo Record Found!!!");
+    }else{
+        printf("\n\t\tGiven Record Deleted Successfully");
+    }
+    // close both file
+    fclose(fp);
+    fclose(temp);
+    // remove main file
+    remove("student_data.txt");
+    // rename the temp file to original file
+    rename("temp_data.txt", "student_data.txt");
     printf("\n\n\t\tPress any key to Continue...");
     getch(); // hold the screen
     menu();
